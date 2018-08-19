f649384105869cc3c0f99ad779b074507c5eab97
Anthony Jimenez <39908020+anthony5226@users.noreply.github.com>
2018-08-19 19:23:17 -0400

    Add files via upload

---
+++ javahard/FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+     public static void main( String[] args ) {
+         System.out.println("I, Anthony Jimenez, am determined to learn how to code.");
+         System.out.println("August 4 2018");
+     }
+    }
